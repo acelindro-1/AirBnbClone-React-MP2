624ac8e4d165ee5e2ea087863bf0fa0c52309a2a
new_acelindro-1 <acelindro1998@gmail.com>
2023-10-21 11:51:13 +0800

    initial commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Avatar() {
+    return ( 
+        <img 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src="../../src/assets/images/placeholder.jpg"
+
+    />
+     );
+}
+
+export default Avatar
